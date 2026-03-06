f6a7e0f99593cef79240cd4b30055e015cb1b44f
JimIIT <akidul201103@gmail.com>
2026-03-07 02:44:27 +0600

    added registerpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/auth/LoginPage'
+import RegisterPage from './pages/auth/RegisterPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<div>Landing Page</div>} />
       <Route path="/login" element= {<LoginPage />} />
-      <Route path="/register" element={<div>Register Page</div>} />
+      <Route path="/register" element= {<RegisterPage/>} />
       <Route path="/patient/dashboard" element={<div>Patient Dashboard</div>} />
       <Route path="/doctor/dashboard" element={<div>Doctor Dashboard</div>} />
     </Routes>
