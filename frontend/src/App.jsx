e8a5232d99427efe20ddde3f562fb1f1988e4e36
JimIIT <akidul201103@gmail.com>
2026-03-07 04:55:48 +0600

    Home page set up done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/auth/LoginPage'
 import RegisterPage from './pages/auth/RegisterPage'
+import LandingPage from './pages/LandingPage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Landing Page</div>} />
+      <Route path="/" element={<LandingPage/>} />
       <Route path="/login" element= {<LoginPage />} />
       <Route path="/register" element= {<RegisterPage/>} />
       <Route path="/patient/dashboard" element={<div>Patient Dashboard</div>} />
